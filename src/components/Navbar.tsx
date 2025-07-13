38ac8e0d0576ad41047815afaa42cb01c252eea8
alzaeemraad <alzaeemraad9@gmail.com>
2025-07-13 23:12:29 +0530

    menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,7 +68,7 @@ const Navbar: React.FC = () => {
     : "bg-transparent";
 
   return (
-    <div className="absolute top-0 left-0 right-0 z-50">
+    <div className="fixed top-0 left-0 right-0 z-50">
       {/* TopBar */}  
       <div className={`${navbarBg} transition-all duration-150`}>
         <TopBar />
